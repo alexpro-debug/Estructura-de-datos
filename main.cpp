9517b5ce8dda296aee111ec1a486b8c0f769d360
Alex <alexpro5098@gmail.com>
2026-09-21 10:28:22 -0600

    feat: agrega seguimiento de pasadas y contadores

--- main.cpp
+++ main.cpp
@@ -3,15 +3,27 @@
 using namespace std;
 
 void burbuja(int numeros[], int n) {
+    int comparaciones = 0;
+    int intercambios = 0;
+
     for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
+            comparaciones++;
             if(numeros[j] > numeros[j+1]) {
                 int temp = numeros[j];
                 numeros[j] = numeros[j+1];
                 numeros[j+1] = temp;
+                intercambios++;
             }
         }
+        cout << "pasada " << i + 1 << ": ";
+        for(int k = 0; k < n; k++) cout << numeros[k] << " ";
+        cout << endl;
     }
+
+    cout << "\nelementos: " << n << endl;
+    cout << "comparaciones: " << comparaciones << endl;
+    cout << "intercambios: " << intercambios << endl;
 }
 
 int main() {
@@ -24,9 +36,5 @@ int main() {
 
     burbuja(arr, n);
 
-    cout << "arreglo ordenado: ";
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << endl;
-    
     return 0;
 }
