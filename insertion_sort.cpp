0fd05f3bfaf61412083f9f2b41b28b4a79c507fe
Alex <alexpro5098@gmail.com>
2026-09-23 10:15:14 -0600

    implementa algoritmo y logica insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -8,12 +8,27 @@ void mostrarArreglo(int numeros[], int n) {
     cout << endl;
 }
 
+void insertionSort(int numeros[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = numeros[i];
+        int j = i - 1;
+        
+        while (j >= 0 && numeros[j] > key) {
+            numeros[j + 1] = numeros[j];
+            j = j - 1;
+        }
+        numeros[j + 1] = key;
+    }
+}
+
 int main() {
     int numeros[] = {7, 3, 8, 2, 6, 4, 5};
     int n = 7;
 
     cout << "Arreglo original:" << endl;
     mostrarArreglo(numeros, n);
+
+    insertionSort(numeros, n);
 
     return 0;
 }
